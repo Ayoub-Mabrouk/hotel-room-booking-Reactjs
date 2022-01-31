93358cd886b9f2cc3dae46776cc795afcd3405ca
naitiaz-aya <bntsaidaya@gmail.com>
2022-01-31 10:07:47 +0100

    adding changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import {LoginPage} from "./LoginPage";
 import {HomePage} from './HomePage'
+import {AboutUsPage} from './AboutUs'
 import {NotFoundPage} from "./NotFoundPage";
-export {LoginPage,HomePage,NotFoundPage};
+export {LoginPage,HomePage,NotFoundPage,AboutUsPage};
